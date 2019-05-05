1aa202a26de86826138f817e92d6a63896b9be0e
koki25ando <basket.koki.bowl@gmail.com>
2019-05-05 14:27:21 +0900

    updates

--- HousePrice.R
+++ HousePrice.R
@@ -2,6 +2,8 @@
 library(tidyverse)
 library(naniar)
 library(simputation)
+library(rpart)
+library(randomForest)
 test = read.csv("test.csv")
 train = read.csv("train.csv")
 
@@ -15,3 +17,29 @@ merged_dat = rbind(train, test)
 vars_int = c("BsmtFullBath", "BsmtHalfBath", "BsmtFinSF1", "BsmtFinSF2", "BsmtUnfSF", "TotalBsmtSF", "GarageCars", "GarageArea")
 vars_factor = c("MSZoning", "Utilities", "Functional", "Exterior1st", "Exterior2nd", "Electrical", "KitchenQual", "SaleType")
 merged_dat = impute_mean_at(merged_dat, vars_int)
+
+
+
+
+
+
+
+
+
+
+# Predict
+train = merged_dat %>% 
+  filter(Train == "YES") %>% 
+  select(-Train)
+test = merged_dat %>% 
+  filter(Train == "NO") %>% 
+  select(-Train, -SalePrice)
+
+model_rpart = rpart(data = train, SalePrice ~ .)
+# model_rf = randomForest(SalePrice ~ ., data = train,
+#                         ntree = 500, importance = TRUE)
+
+
+test$SalePrice = predict(model_rf, test)
+submission = select(test, Id, SalePrice)
+write.csv(submission, "submission.csv", row.names = FALSE)
