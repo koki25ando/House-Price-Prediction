c359103a848ab292ccafa0b404805e15d02de72a
koki25ando <basket.koki.bowl@gmail.com>
2019-05-05 14:25:45 +0900

    updates

--- HousePrice.R
+++ HousePrice.R
@@ -1,4 +1,17 @@
 # preparation
 library(tidyverse)
+library(naniar)
+library(simputation)
 test = read.csv("test.csv")
 train = read.csv("train.csv")
+
+# combining datasets
+test$SalePrice = 0
+train$Train = "YES"
+test$Train = "NO"
+merged_dat = rbind(train, test)
+
+# imputing missing values
+vars_int = c("BsmtFullBath", "BsmtHalfBath", "BsmtFinSF1", "BsmtFinSF2", "BsmtUnfSF", "TotalBsmtSF", "GarageCars", "GarageArea")
+vars_factor = c("MSZoning", "Utilities", "Functional", "Exterior1st", "Exterior2nd", "Electrical", "KitchenQual", "SaleType")
+merged_dat = impute_mean_at(merged_dat, vars_int)
